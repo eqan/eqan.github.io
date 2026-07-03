39633595db884a21ab45e563d0a078506460cf37
Eqan Ahmad <eqan.ahmad123@gmail.com>
2026-07-03 19:07:17 +0200

    main: loading microsoft clarity stats instantly

--- JS/lazy-loader.js
+++ JS/lazy-loader.js
@@ -43,18 +43,6 @@
     idle: idle
   };
 
-  window.addEventListener('load', function () {
-    if (location.hostname === 'eqanahmad.com' || location.hostname === 'www.eqanahmad.com') {
-      idle(function () {
-        (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "hbem75z47m");
-      }, 5000);
-    }
-  });
-
   if (window.matchMedia && window.matchMedia('(hover: hover) and (pointer: fine)').matches) {
     idle(function () {
       loadStyle('./CSS/cursor.css');
